5286e370af873ebaa514971a438ed776c8a142ab
Brendan <brendantsuda@gmail.com>
2022-10-28 16:16:57 -0700

    deployment to render

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module LifeliftCoffee
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.railties_order = [:all, :main_app]
     config.active_storage.draw_routes = false
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore,
