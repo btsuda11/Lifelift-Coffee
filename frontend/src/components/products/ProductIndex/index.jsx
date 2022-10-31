7d88eb581057c63cb8952627e6eb5e5e7afd05c4
Brendan <brendantsuda@gmail.com>
2022-10-31 10:28:38 -0700

    added header based on category

--- frontend/src/components/products/ProductIndex/index.jsx
+++ frontend/src/components/products/ProductIndex/index.jsx
@@ -3,10 +3,23 @@ import { useDispatch, useSelector } from 'react-redux';
 import ProductIndexItem from './ProductIndexItem';
 import { getProducts, fetchProducts } from '../../../actions/productActions';
 import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ProductIndex = () => {
     const dispatch = useDispatch();
     const products = useSelector(getProducts);
+
+    let { category } = useParams();
+
+    const capitalizeWords = str => {
+        const words = str.split('-');
+        const capitalizedWords = words.map((word) => (
+            word[0].toUpperCase() + word.slice(1).toLowerCase()
+        ))
+        return capitalizedWords.join(' ');
+    }
+
+    category ? category = capitalizeWords(category) : category = 'All Products';
 
     useEffect(() => {
         dispatch(fetchProducts());
@@ -18,6 +31,14 @@ const ProductIndex = () => {
                 <div>
                     <h1>Shop Lifelift</h1>
                     <p className='spaced'>Using Sustainable Farming & Protecting Our Wildlife</p>
+                </div>
+            </section>
+            <section className='products-section'>
+                <div>
+                    <h2>{category}</h2>
+                </div>
+                <div>
+
                 </div>
             </section>
         </>
