cac89a038facef270dfb561a2ede374e5888dea8
Brendan <brendantsuda@gmail.com>
2022-11-02 20:03:25 -0700

    refactored each option as own product

--- frontend/src/components/products/ProductShow/ProductInfo/index.jsx
+++ frontend/src/components/products/ProductShow/ProductInfo/index.jsx
@@ -8,6 +8,8 @@ const ProductInfo = ({ product, spotlight }) => {
     const [clickType, setClickType] = useState({'Ground': true, 'Whole Beans': false, 'Go Bags': false});
     const [typeStyle, setTypeStyle] = useState({'Ground': {backgroundColor: '#e5e7eb'}, 'Whole Beans': {}, 'Go Bags': {}});
 
+    const productTypes = [...new Set(product.map(option => option.productType))];
+
     const handleQuantity = quantity => {
         const quant = { '1': false, '3': false, '6': false };
         const quantStyle = {'1': {}, '3': {}, '6': {}};
@@ -35,39 +37,28 @@ const ProductInfo = ({ product, spotlight }) => {
                 <button className='product-toggle-btn bold'>Buy One Time</button>
                 <div>
                     <div className='product-type-div'>
-                        {product.map(type => {
-                            if (type.productType) return <button onClick={() => handleType(type.productType)} style={typeStyle[type.productType]} className='product-type-btn'>{type.productType}</button>
+                        {productTypes.map(type => {
+                            if (type) return <button onClick={() => handleType(type)} style={typeStyle[type]} className='product-type-btn'>{type}</button>
                         })}
                     </div>
-                    {product.map(type => {
-                        return (
-                            <>
-                                { (clickType[type.productType] === true || !type.productType) &&
-                                    <div className='price-btn-div'>
-                                        <button onClick={() => handleQuantity('1')} className='product-price-btn' style={quantityStyle['1']}>
-                                            <p className='bold'>1</p>
-                                            <p className='bold'>${(type.price).toFixed(2)}</p>
-                                            <p className='strike-out'>${(1.25 * type.price).toFixed(2)}</p>
+                    <div className='price-btn-div'>
+                        {product.map(option => {
+                            return (
+                                <>
+                                    {(clickType[option.productType] === true || !option.productType) &&
+                                        <button onClick={() => handleQuantity(option.amount)} className='product-price-btn' style={quantityStyle[option.amount]}>
+                                            <p className='bold'>{option.amount}</p>
+                                            <p className='bold'>${(option.price / option.amount).toFixed(2)}</p>
+                                            {option.amount === 1 ? <p className='strike-out'>${(1.25 * option.price).toFixed(2)}</p> : null }
+                                            {option.category === 'Coffee Pods' && option.amount !== 1 ? <p>Per Box</p> : null}
+                                            {option.category === 'Health Boosters' && option.amount !== 1 ? <p>Per Unit</p> : null}
+                                            {(option.category === 'Light Medium Dark Roasts' || option.category === 'Decaf Coffee' || option.category === 'Flavored Coffee') && option.amount !== 1 ? <p>Per Bag</p> : null}
                                         </button>
-                                        <button onClick={() => handleQuantity('3')} className='product-price-btn' style={quantityStyle['3']}>
-                                            <p className='bold'>3</p>
-                                            <p className='bold'>${(0.89 * type.price).toFixed(2)}</p>
-                                            {type.category === 'Coffee Pods' ? <p>Per Box</p> : null}
-                                            {type.category === 'Health Boosters' ? <p>Per Unit</p> : null}
-                                            {type.category === 'Light Medium Dark Roasts' || type.category === 'Decaf Coffee' || type.category === 'Flavored Coffee' ? <p>Per Bag</p> : null}
-                                        </button>
-                                        <button onClick={() => handleQuantity('6')} className='product-price-btn' style={quantityStyle['6']}>
-                                            <p className='bold'>6</p>
-                                            <p className='bold'>${(0.82 * type.price).toFixed(2)}</p>
-                                            {type.category === 'Coffee Pods' ? <p>Per Box</p> : null}
-                                            {type.category === 'Health Boosters' ? <p>Per Unit</p> : null}
-                                            {type.category === 'Light Medium Dark Roasts' || type.category === 'Decaf Coffee' || type.category === 'Flavored Coffee' ? <p>Per Bag</p> : null}
-                                        </button>
-                                    </div>
-                                }
-                            </>
-                        )
-                    })}
+                                    }
+                                </>
+                            )
+                        })} 
+                    </div>
                 </div>
                 <button className='red-btn'>Add to Cart</button>
             </div>
