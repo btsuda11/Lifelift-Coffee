1c822921f2fd71ec4ecc58c3e4ceb769f6174e02
Brendan <brendantsuda@gmail.com>
2022-11-10 09:48:05 -0800

    finished our story page

--- frontend/src/components/our-story/index.jsx
+++ frontend/src/components/our-story/index.jsx
@@ -1,5 +1,6 @@
 import './OurStoryPage.css';
 import ritualImg from '../../assets/OurStoryPage/ritual-img.jpg';
+import profilePic from '../../assets/OurStoryPage/profile-pic.JPG';
 
 const OurStoryPage = ({ closeCart }) => {
     return (
@@ -12,6 +13,14 @@ const OurStoryPage = ({ closeCart }) => {
                     <p>We LOVE coffee. It's a ritual that starts your day off and brings you to that happy place of energy, productivity, focus, and emotional wellbeing.</p>
                     <p>If you want to feel your best, then why not feed your body the best source that you can find? That's why Lifelift Coffee was born.</p>
                 </div>
+            </section>
+            <section onClick={closeCart} className='personal-section'>
+                <div>
+                    <h1>Spill the "Coffee"</h1>
+                    <p>Thank you to <span className='bold'>Lifeboost Coffee</span> for inspiring this website clone! Funny enough, I'm not even an avid coffee drinker. I just started drinking coffee and Lifeboost Coffee was one of the first brands I stumbled upon. My name is Brendan Tsuda, a former chemist turned full-stack software engineer. I have experience with JavaScript, React, Redux, Ruby on Rails, and PostgreSQL. I am interested in developing and maintaining web applications that impact our daily lives. Hope you enjoyed browsing this website as much as I did creating it!</p>
+                    <p className='bold'>- Brendan Tsuda</p>
+                </div>
+                <img src={profilePic} />
             </section>
         </>
     )
